13e958e399096d244ee4ddbfa061dd34b5b246a6
Riaz Moola <riaz.moola@elektron-technology.com>
2018-02-26 11:14:37 +0000

    Added basic class based promise wrapper

--- promiseWrapper.js
+++ promiseWrapper.js
@@ -6,43 +6,83 @@ var options = {
   port: credentials.TESTPort()
 };
 
-var connect = (options) => {
+// var connect = (options) => {
 
-  return new Promise((resolve, reject) => {
-    var SSHClient = require('./lib/index.js')(options);
-    SSHClient.on('stderr', function(err) { console.log("Error on StdErr",err); });
-    SSHClient.on('output', function(data,lastChunk) { 
-	    if (lastChunk == true) {
-		    console.log("Should close connection");
-		    SSHClient.close();
-	    }
-	    if (data) {
-		    if (data.match("pwd")) { console.log("Command data->",data);
-		                           }else { console.log("Output data->",data); }
-	    }
-    });
-    //Connection
-    SSHClient.once('closed', function(addr,err) { 
-	    if (err) {
-		    console.log("Error on session",err); 
-	    }else { console.log("SSH Session has being closed."); }
-    });
-    SSHClient.on('connected',(host) => {
-      console.log('Were Connected',host);
-      SSHClient.write('pwd');
+//   return new Promise((resolve, reject) => {
+//     var SSHClient = require('./lib/index.js')(options);
+//     SSHClient.on('stderr', function(err) { console.log("Error on StdErr",err); });
+//     SSHClient.on('output', function(data,lastChunk) { 
+// 	    if (lastChunk == true) {
+// 		    console.log("Should close connection");
+// 		    SSHClient.close();
+// 	    }
+// 	    if (data) {
+// 		    if (data.match("pwd")) { console.log("Command data->",data);
+// 		                           }else { console.log("Output data->",data); }
+// 	    }
+//     });
+//     //Connection
+//     SSHClient.once('closed', function(addr,err) { 
+// 	    if (err) {
+// 		    console.log("Error on session",err); 
+// 	    }else { console.log("SSH Session has being closed."); }
+//     });
+//     SSHClient.on('connected',(host) => {
+//       console.log('Were Connected',host);
+//       SSHClient.write('pwd');
+//       resolve()
+//     });
+//     SSHClient.connect();
+//     //  resolve('uh connected I think')
+//   });
+// };
 
-      resolve()
-    });
-    SSHClient.connect();
-    //  resolve('uh connected I think')
-  });
-};
+// connect(options).then((string) => {
+//   console.log('amazing');
+//   console.log(string);
+// });
 
-connect(options).then((string) => {
-  console.log('amazing');
-  console.log(string);
-});
-debugger;
+
+class SSH {
+  constructor(options) {
+    this.connection = null;
+    this.options = options;
+  }
+  connect() {
+    return new Promise((resolve, reject) => {
+      this.connection = require('./lib/index.js')(this.options);
+      this.connection.on('stderr', function(err) { console.log("Error on StdErr",err); });
+      this.connection.on('output', function(data,lastChunk) { 
+	      if (lastChunk == true) {
+		      console.log("Should close connection");
+		      this.connection.close();
+	      }
+	      if (data) {
+		      if (data.match("pwd")) { console.log("Command data->",data);
+		                             }else { console.log("Output data->",data); }
+	      }
+      });
+      //Connection
+      this.connection.once('closed', function(addr,err) {
+	      if (err) {
+		      console.log("Error on session",err);
+	      }else { console.log("SSH Session has being closed."); }
+      });
+      this.connection.on('connected',(host) => {
+        console.log('Were Connected',host);
+        this.connection.write('pwd');
+        resolve();
+      });
+      this.connection.connect();
+    });
+  }
+}
+
+
+var mySSH = new SSH(options);
+mySSH.connect();
+
+
 //connect(options).
 
 // class SSH {
